80e115158de4ecb53fab6bf8997b69d8872749da
ChrigorG <chrigor81@gmail.com>
2023-04-27 00:14:22 -0400

    Instalando o componete TinyMCE na pag articles new

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const category = require("../categories/Category");
+
+//Method GET
 
 router.get("/articles", (req, resp) => {
     resp.send("Articles");
+});
+
+router.get("/admin/articles/new", (req, resp) => {
+    category.findAll().then(category => {
+        resp.render("admin/articles/new", {category:category});
+    }).catch(erro => {
+        resp.redirect("/admin/articles");
+    });
 });
 
 module.exports = router;
