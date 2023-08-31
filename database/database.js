17a276fa45f8c839bda01505124c56b59e9cf749
ChrigorG <99369312+ChrigorG@users.noreply.github.com>
2023-08-31 19:57:00 -0400

    Atualização no database.js

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
-const connection = new Sequelize("guiapress", "root", "L@zag1228", {
+const connection = new Sequelize("guiapress", "root", "sua-senha", {
     host: "127.0.0.1",
     dialect: "mysql",
-    timezone: "-04:00"    // para salvar a data baseado no fuso horario
+    timezone: "-03:00"    // para salvar a data baseado no fuso horario
 });
 
 module.exports = connection;
