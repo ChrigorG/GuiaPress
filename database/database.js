1c19c0d9d9b77c760f89bf18d9e572b62bdc6311
ChrigorG <99369312+ChrigorG@users.noreply.github.com>
2023-08-31 20:07:27 -0400

    Delete database directory

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-const connection = new Sequelize("guiapress", "root", "sua-senha", {
-    host: "127.0.0.1",
-    dialect: "mysql",
-    timezone: "-03:00"    // para salvar a data baseado no fuso horario
-});
-
-module.exports = connection;
